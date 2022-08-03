187f84174dd5859814067e19ad251efcc7391b77
IgorPlakhov <pie150881@gmail.com>
2022-08-03 21:36:36 +0300

    gh-pages3 added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,10 @@ function Header() {
                     <a href="/" className="brand-logo">React Shop</a>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
                         <li>
-                            <a href="https://github.com/IgorPlakhov/react-shop"  target="_blank" rel="noreferrer">Repo</a>
+                            <a
+                                href="https://github.com/IgorPlakhov/react-shop"
+                                target="_blank"
+                                rel="noreferrer">Repo</a>
                         </li>                    
                     </ul>
                 </div>
