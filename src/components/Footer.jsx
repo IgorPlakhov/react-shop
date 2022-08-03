e5beaeefdbf0256904a4abfacb8a66954b054522
IgorPlakhov <pie150881@gmail.com>
2022-08-03 19:55:17 +0300

    gh-pages added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,14 @@ function Footer() {
                 <div className="footer-copyright">
                     <div className="container">
                         © {new Date().getFullYear()} Copyright Text
-                        <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+                        <a
+                            className="grey-text text-lighten-4 right"
+                            href="https://github.com/IgorPlakhov/react-shop"
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                            Repo
+                        </a>
                     </div>
                 </div>
             </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,11 @@ function Header() {
         <div>
             <nav className="green darken-1">
                 <div className="nav-wrapper">
-                    <a href="#!" className="brand-logo">React Shop</a>
+                    <a href="/" className="brand-logo">React Shop</a>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
-                        <li><a href="!#">Repo</a></li>                    
+                        <li>
+                            <a href="https://github.com/IgorPlakhov/react-shop"  target="_blank" rel="noreferrer">Repo</a>
+                        </li>                    
                     </ul>
                 </div>
             </nav>
